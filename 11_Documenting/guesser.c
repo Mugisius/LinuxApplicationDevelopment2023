581791d36270e0aa9104f0aee1c5afd8a83682cf
Akinin Evgeniy <olympiads@inbox.ru>
2023-12-05 21:18:10 +0300

    Task 11 Documenting

---
+++ 11_Documenting/guesser.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <libgen.h>
+#include <libintl.h>
+#include <locale.h>
+#include <string.h>
+#include "config.h"
+
+#define _(STRING) gettext(STRING)
+#define LOCALE_PATH "."
+
+int numPrint(int n)
+{
+    
+}
+
+int main(int argc, char *argv[])
+{
+    char *dir;
+    char *buf = NULL;
+    size_t len = 0;
+
+    if (argc > 2)
+    {
+        printf("Usage: %s [-r]", argv[0]);
+        exit(1);
+    }
+    if (argc == 2)
+        if (strcmp(argv[1], "-r") == 0)
+            using_roman = 1;
+        else
+        {
+            printf("%s: Wrong argument: %s", argv[0], argv[1]);
+            exit(2);
+        }
+
+    dir = dirname(realpath(argv[0], NULL));
+    setlocale (LC_ALL, "");
+    bindtextdomain ("guesser", LOCALE_PATH);
+    textdomain ("guesser");
+
+    puts(_("Think of a number from 1 to 100.\n"));
+    
+    int low = 1;
+    int high = 100;    
+
+    while (low != high)
+    {
+        int step = (low+high)/2;
+        printf(_("Is your number greater than %d? [y/n]:"), step);
+        if (getline(&buf, &len, stdin))
+        {
+            if (strcmp(buf, _("y\n")) == 0)
+                low = step+1;
+            else if (strcmp(buf, _("n\n")) == 0)
+                high = step;
+        }
+        
+    }
+    printf(_("Your number is %d!\n"), low);
+
+    return 0;
+}
